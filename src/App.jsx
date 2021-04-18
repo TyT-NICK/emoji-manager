2d93b32d67462e4746d6c0444887cb04a5d668c4
TyT-NICK <artem99k14@gmail.com>
2021-04-19 01:44:44 +0800

    added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import useHttp from './Hooks/useHttp';
 
 function App() {
+  const [data, setData] = useState('');
+  const { loading, request } = useHttp();
+
+  // useEffect(() => {
+  //   const fetching = async () => {
+  //     const fetched = await request('https://api.github.com/emojis', 'GET');
+
+  //     setData(JSON.stringify(fetched));
+  //   };
+
+  //   fetching();
+  // }, []);
+
   return (
     <div className="App">
-      1
+      {
+        loading ? 'loading...' : data
+      }
     </div>
   );
 }
--- src/Hooks/useHttp.js
+++ src/Hooks/useHttp.js
@@ -1,5 +1,29 @@
-const useHttp = () => {
-  // const
-};
+import { useCallback, useState } from 'react';
 
-export default useHttp;
+export default function useHttp() {
+  const [loading, setLoading] = useState(false);
+
+  const request = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
+    setLoading(true);
+
+    try {
+      const requestBody = body ? JSON.stringify(body) : null;
+
+      const response = await fetch(url, { method, headers, requestBody });
+      const data = response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+
+      setLoading(false);
+
+      return data;
+    } catch (e) {
+      setLoading(false);
+      throw e;
+    }
+  });
+
+  return { loading, request };
+}
