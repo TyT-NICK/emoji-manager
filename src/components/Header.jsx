1b500cd877265c53d0252857dc385849f2ee9722
TyT-NICK <artem99k14@gmail.com>
2021-04-22 18:23:09 +0800

    base structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,17 @@ const Header = (props) => {
   const { title } = props;
   return (
     <header>
-      <div id="mobile-nav-button" />
-      {title}
+      <nav>
+        <div className="nav-wrapper">
+          <div id="mobile-nav-button" />
+          <a href="#!" className="brand-logo">{title}</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+          </ul>
+        </div>
+      </nav>
     </header>
   );
 };
